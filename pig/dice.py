8725b4c9c355edc6715be5b7c4f20b694f2c2c0c
Gabriel <gabriel.thiman0007@stud.hkr.se>
2023-02-16 16:02:14 +0100

    Creates print method and testFile

--- pig/dice.py
+++ pig/dice.py
@@ -1,13 +1,70 @@
+class Dice:
 
+    def __init__(self):
+        pass
 
-def __init__(self):
-    pass
+    __value = 0
 
+    def getValue(self):
+        return self.__value
 
-__value = 0
+    def setValue(self, newValue):
+        self.__value = newValue
+        print(self.__value)
 
+    def printFace(self):
+        if self.__value != 0:
+            if self.__value == 1:
+                print("""
+                 ---------
+                |         |
+                |    *    |
+                |         |
+                 ---------
+                """)
+            elif self.__value == 2:
+                print("""
+                 ---------
+                |  *      |
+                |         |
+                |      *  |
+                 ---------
+                """)
+            elif self.__value == 3:
+                print("""
+                 ---------
+                |  *      |
+                |    *    |
+                |      *  |
+                 ---------
+                """)
+            elif self.__value == 4:
+                print("""
+                 ---------
+                |  *   *  |
+                |         |
+                |  *   *  |
+                 ---------
+                """)
+            elif self.__value == 5:
+                print("""
+                 ---------
+                |  *   *  |
+                |    *    |
+                |  *   *  |
+                 ---------
+                """)
+            else:
+                print("""
+                 ---------
+                |  *   *  |
+                |  *   *  |
+                |  *   *  |
+                 ---------
+                """)
 
-def getValue():
-    return __value
+    def main():
+        pass
 
-
+    if __name__ == '__main__':
+        main()
---
+++ pig/testFile.py
@@ -0,0 +1,11 @@
+from dice import Dice
+
+
+def main():
+    dc = Dice()
+    dc.setValue(6)
+    dc.printFace()
+
+
+if __name__ == '__main__':
+    main()
