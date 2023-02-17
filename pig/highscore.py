2bc091b509f8def0d3919459e193665f7c12f2e7
Anton Bryhagen <anton.bryhagen@gmail.com>
2023-02-17 22:38:48 +0100

    Add highscore class

---
+++ pig/highscore.py
@@ -0,0 +1,17 @@
+#From player import Player
+
+class Highscore:
+    def __init__(self):
+        pass
+
+    def add_highscore():
+        pass
+
+    def get_highscores():
+        pass
+        #return table with scores
+        
+
+    #Add new highscore
+    #Read highscores
+    #Store in table with Player obj and high score value
