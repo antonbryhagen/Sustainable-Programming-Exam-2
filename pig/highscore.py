fe5b0cdfa972fa70f216c911eb4e2a8ce6e18e4c
Anton <anton.bryhagen@gmail.com>
2023-02-20 14:20:00 +0100

    Start of implementation update_highscore method

--- pig/highscore.py
+++ pig/highscore.py
@@ -20,10 +20,17 @@ class Highscore:
     
     def update_highscore(self, player: Player):
         highscores = self.get_highscores()
-        pass
+        
+        if player in highscores:
+            highscores[player] = [highscores[player]] #get stats and update wins and games played
+        else:
+            highscores[player] = [1] #add new player and games played
+            
     #check for name in file, if name is there, update highscore
     #if name is not there, add it
 
     #Add new highscore
     #Read highscores
     #Store in table with Player obj and high score value
+    
+    #get as string / list method
