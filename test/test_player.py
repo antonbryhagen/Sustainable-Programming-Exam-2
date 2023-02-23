0ba38bcc268205af65d85e98f0e403a85fb976b2
Anton Bryhagen <anton.bryhagen@gmail.com>
2023-02-23 18:29:31 +0100

    Add unit tests for player class

--- test/test_player.py
+++ test/test_player.py
@@ -7,6 +7,45 @@ class TestPlayerClass(unittest.TestCase):
         res = player.Player("Username")
         exp = player.Player
         self.assertIsInstance(res, exp)
+    
+    def test_get_name(self):
+        test_player = player.Player("Username")
+        res = test_player.get_name()
+        exp = "Username"
+        self.assertEqual(res, exp)
+
+    def test_get_score(self):
+        test_player = player.Player("Username")
+        res = test_player.get_score()
+        exp = 0
+        self.assertEqual(res, exp)
+
+    def test_get_current_turn_score(self):
+        test_player = player.Player("Username")
+        res = test_player.get_current_turn_score()
+        exp = 0
+        self.assertEqual(res, exp)
+    
+    def test_set_name(self):
+        test_player = player.Player("Username")
+        test_player.set_name("Name1")
+        res = test_player._name
+        exp = "Name1"
+        self.assertEqual(res, exp)
+
+    def test_set_score(self):
+        test_player = player.Player("Username")
+        test_player.set_score(5)
+        res = test_player._score
+        exp = 5
+        self.assertEqual(res, exp)
+
+    def test_set_current_turn_score(self):
+        test_player = player.Player("Username")
+        test_player.set_current_turn_score(5)
+        res = test_player._current_turn_score
+        exp = 5
+        self.assertEqual(res, exp)
 
 
 if __name__ == "__main__":
