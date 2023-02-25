922216093a6bf8338b4096f856f990b6fc027399
Anton Bryhagen <anton.bryhagen@gmail.com>
2023-02-25 17:35:41 +0100

    Finalize update_highscore method

--- pig/highscore.py
+++ pig/highscore.py
@@ -3,34 +3,44 @@ import pickle
 
 class Highscore:
     def __init__(self):
-        pass
+        _highscores = dict()
 
     def get_highscores(self):
         #create dictionary if first time running game on new system
-        highscores = dict()
+        
         with open('highscores.bin', 'rb') as highscores_file:
             try:
-                highscores = pickle.load(highscores_file)
+                self._highscores = pickle.load(highscores_file)
             except EOFError:    #if no highscores (empty file), use empty dictionary
                 pass
-        return highscores
 
         
         #return table with scores from file
     
-    def update_highscore(self, player: Player):
-        highscores = self.get_highscores()
-        
-        if player in highscores:
-            highscores[player] = [highscores[player]] #get stats and update wins and games played
+    def update_highscore(self, player: Player, won: bool):
+        #check for name in file, if name is there, update highscore
+        #if name is not there, add it
+        if player in self._highscores:
+            wins = self._highscores[player][0] #get wins from list stored in dict
+            games_played = self._highscores[player][1] + 1 #get played games from list stored in dict and add 1
+            if won:
+                wins += 1
+            self._highscores[player] = [wins, games_played] #update wins and games played
         else:
-            highscores[player] = [1] #add new player and games played
-            
-    #check for name in file, if name is there, update highscore
-    #if name is not there, add it
-
-    #Add new highscore
-    #Read highscores
-    #Store in table with Player obj and high score value
-    
-    #get as string / list method
+            if won:
+                highscore = [1, 1] # 1 win, 1 game played
+            else:
+                highscore = [0, 1] # 0 wins, 1 game played
+            self._highscores[player] = highscore #add new player and games played
+        with open('highscores.bin', 'wb') as highscore_file:
+            try:
+                pickle.dump(self._highscores, highscore_file)
+            except IOError:
+                print("Error writing saving highscores")
+        #get as string / list method
+        #highscore[0] = wins, highscore[1] = games played. stored in list in dict
+    def __str__(self):
+        highscore_string = ""
+        for player, highscore in self._highscores:
+            highscore_string += f'Name: {player.get_name()}, Wins: {highscore[0]}, Games played: {highscore[1]}\n'
+        return
