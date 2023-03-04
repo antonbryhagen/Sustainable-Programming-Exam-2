92a809fa303f5c7af05415855e69ab93128058b1
Anton Bryhagen <anton.bryhagen@gmail.com>
2023-03-04 18:08:43 +0100

    Refactor player.py according to flake8 standards

--- pig/player.py
+++ pig/player.py
@@ -1,12 +1,12 @@
 class Player:
     def __init__(self, name):
         self._name = name
-        self._score = 0 # Start player score at zero
-        self._current_turn_score = 0 # Set current turn score to zero
+        self._score = 0  # Start player score at zero
+        self._current_turn_score = 0  # Set current turn score to zero
 
     def get_name(self):
         return self._name
-    
+
     def set_name(self, name):
         self._name = name
 
@@ -15,10 +15,9 @@ class Player:
 
     def set_score(self, score):
         self._score = score
-    
+
     def get_current_turn_score(self):
         return self._current_turn_score
 
     def set_current_turn_score(self, turn_score):
         self._current_turn_score = turn_score
-    
