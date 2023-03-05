e0923c5ab1287b39c10ef5135a264f5e6dfca43f
Anton Bryhagen <anton.bryhagen@gmail.com>
2023-03-05 15:37:17 +0100

    Add doc strings to highscore.py

--- pig/highscore.py
+++ pig/highscore.py
@@ -3,48 +3,43 @@ from pig import player
 
 
 class Highscore:
+    """Highscore class."""
+
     def __init__(self, path):
+        """Init the object"""
         self._highscores = {}
         self._path = path
 
     def get_highscores(self):
-        # create dictionary if first time running game on new system
-
+        """Load highscore dictionary from highscore file."""
         with open(self._path, 'rb') as highscores_file:
             try:
                 self._highscores = pickle.load(highscores_file)
-            except EOFError:  # if no highscores (empty file), use empty dict
+            except EOFError:
                 pass
 
-        # return table with scores from file
-
     def update_highscore(self, user: player.Player, won: bool):
-        # check for name in file, if name is there, update highscore
-        # if name is not there, add it
+        """Update highscore dictionary and write to highscore file."""
         self.get_highscores()
+        # dictionary -> {name: [wins, games_played]}
         if user.get_name() in self._highscores:
-            # get wins from list stored in dict
             wins = self._highscores[user.get_name()][0]
-            # get played games from list stored in dict and add 1
             games_played = self._highscores[user.get_name()][1] + 1
             if won:
                 wins += 1
-            # update wins and games played
             self._highscores[user.get_name()] = [wins, games_played]
         else:
             if won:
-                highscore = [1, 1]  # 1 win, 1 game played
+                highscore = [1, 1]
             else:
-                highscore = [0, 1]  # 0 wins, 1 game played
-            # add new player and games played
+                highscore = [0, 1]
             self._highscores[user.get_name()] = highscore
+
         with open(self._path, 'wb') as highscore_file:
             pickle.dump(self._highscores, highscore_file)
-        # get as string / list method
-        # highscore[0] = wins, highscore[1] = games played. stored in
-        # list in dict
 
     def __str__(self):
+        """Get highscores as a formatted string"""
         highscore_string = ""
         self.get_highscores()  # Get latest highscores before printing
         for user, highscore in self._highscores.items():
