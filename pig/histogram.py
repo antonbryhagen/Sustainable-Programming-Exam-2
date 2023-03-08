afdf4b91b5bb9bb4d3f9a26b1e753ff8ba036d31
Anton Bryhagen <anton.bryhagen@gmail.com>
2023-03-08 13:19:20 +0100

    Histogram feature implemented

--- pig/histogram.py
+++ pig/histogram.py
@@ -7,41 +7,35 @@ class Histogram:
 
     def __init__(self):
         """Init the object."""
-        self._occurrence_dict = {}
         self._percentage_dict = {}
         self._rounded_percentage_dict = {}
 
-    def _count_occurrence(self, dice_history):
-        """Calculate occurrence of dice faces from dice history."""
-        self._occurrence_dict = {face: dice_history.count(face) for face in dice_history}
 
     def _calculate_percentage(self, dice_history):
         """Calculate ocurrence of dice face in percentage."""
         self._percentage_dict = {face: dice_history.count(face)/len(dice_history) for face in dice_history}
         for key in self._percentage_dict.keys():
-            self._rounded_percentage_dict[key] = math.floor(self._percentage_dict[key]*10)
+            self._rounded_percentage_dict[key] = math.ceil(self._percentage_dict[key]*10)
 
     def get_histogram(self, dice_history):
         """Get histogram as a formatted string."""
-        self._count_occurrence(dice_history)
         self._calculate_percentage(dice_history)
-        bar_list = ['']*6
-        for i, key in enumerate(self._rounded_percentage_dict.keys()):
-            bar_list[i] = '*\n'*self._rounded_percentage_dict[key]
-            bar_list[i] = bar_list[i] + '*'
-        return bar_list
+        chart = ''
+        for i in range(10, 0, -1):
+            row = ''
+            for j in range(1, 7):
+                if self._rounded_percentage_dict[j] >= i:
+                    if j == 1:
+                        row += f'{(i-1)*10:3}-{i*10:3}% | * '
+                    else:
+                        row += ' * '
+                else:
+                    if j == 1:
+                        row += f'{(i-1)*10:3}-{i*10:3}% |   '
+                    else:
+                        row += '   '
+            chart += row + '\n'
+        chart += ('------------------------------\n'
+                  '    Face | 1  2  3  4  5  6')
 
-def main():
-    faces = [2, 5, 6, 2, 2, 4, 5, 5, 5, 3, 3, 3, 4]
-    histo = Histogram()
-    histo._count_occurrence(faces)
-    histo._calculate_percentage(faces)
-    print(histo._occurrence_dict)
-    print(histo._percentage_dict)
-    print(histo._rounded_percentage_dict)
-
-    bars = histo.get_histogram(faces)
-    print(f"{bars[0]}, {bars[1]}")
-
-if __name__ == "__main__":
-    main()
+        return chart
