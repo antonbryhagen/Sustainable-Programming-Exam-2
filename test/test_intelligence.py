7b7fa549dfc0ab5d56a34038eddaa8fab0cfe4df
Anton Bryhagen <anton.bryhagen@gmail.com>
2023-03-09 22:03:54 +0100

    Update docstring

--- test/test_intelligence.py
+++ test/test_intelligence.py
@@ -7,6 +7,7 @@ class TestGameClass(unittest.TestCase):
     """Test the class."""
 
     def test_play(self):
+        """Test play functionallity"""
         play = intelligence.Intelligence()
         dh = dice_hand.Dice_hand()
         action = play.play("1", dh, 0, 0)
